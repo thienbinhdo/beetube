70ce5f727456873b0ac8faf3c1d32f032259adf5
thienbinhdo <thanhbinh.tkt@gmail.com>
2014-11-08 11:17:38 +0700

    delete comment facebook

--- sites/all/themes/beetube_theme/templates/node.tpl.php
+++ sites/all/themes/beetube_theme/templates/node.tpl.php
@@ -105,13 +105,4 @@
 
  <?php print $node->comment; ?>
   
-  <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-  <div class="fb-comments fb_iframe_widget" data-href="<?php print $node_url; ?>" data-width="870" data-numposts="5" data-colorscheme="light"></div>
 </div>
